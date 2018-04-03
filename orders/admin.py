64aa6f3d2cd48849f0eb4022edf335023b607c8f
Gabriel Sanna <gabrielsanna1@gmail.com>
2018-04-03 18:42:46 -0400

    Initial draft of models/migrations

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+class PizzaToppingAdmin(admin.ModelAdmin):
+	pass
+
+admin.site.register(RegularPizza)
+admin.site.register(SicilianPizza)
+admin.site.register(PizzaTopping, PizzaToppingAdmin)
+admin.site.register(Sub)
+admin.site.register(Pasta)
+admin.site.register(Salad)
+admin.site.register(DinnerPlatter)
+
--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,82 @@
 from django.db import models
 
 # Create your models here.
+class RegularPizza(models.Model):
+	LARGE = 'Large'
+	SMALL = 'Small'
+	SIZE_CHOICES = (
+		(LARGE, 'Large'),
+		(SMALL, 'Small'),
+	)
+
+	name = models.CharField(max_length=100)
+	price = models.DecimalField(max_digits=5, decimal_places=2)
+	size = models.CharField(max_length=5, choices=SIZE_CHOICES, default=SMALL)
+
+	def __str__(self):
+		return f"{self.name} ({self.size}): ${self.price}"
+
+class SicilianPizza(models.Model):
+	LARGE = 'Large'
+	SMALL = 'Small'
+	SIZE_CHOICES = (
+		(LARGE, 'Large'),
+		(SMALL, 'Small'),
+	)
+
+	name = models.CharField(max_length=100)
+	price = models.DecimalField(max_digits=5, decimal_places=2)
+	size = models.CharField(max_length=5, choices=SIZE_CHOICES, default=SMALL)
+
+	def __str__(self):
+		return f"{self.name} ({self.size}): ${self.price}"
+
+class PizzaTopping(models.Model):
+	name = models.CharField(max_length=100)
+
+	def __str__(self):
+		return f"{self.name}"
+
+class Sub(models.Model):
+	LARGE = 'Large'
+	SMALL = 'Small'
+	SIZE_CHOICES = (
+		(LARGE, 'Large'),
+		(SMALL, 'Small'),
+	)
+
+	name = models.CharField(max_length=100)
+	price = models.DecimalField(max_digits=5, decimal_places=2)
+	size = models.CharField(max_length=5, choices=SIZE_CHOICES, default=SMALL)
+
+	def __str__(self):
+		return f"{self.name} ({self.size}): ${self.price}"
+
+class Pasta(models.Model):
+	name = models.CharField(max_length=100)
+	price = models.DecimalField(max_digits=5, decimal_places=2)
+
+	def __str__(self):
+		return f"{self.name}: ${self.price}"
+
+class Salad(models.Model):
+	name = models.CharField(max_length=100)
+	price = models.DecimalField(max_digits=5, decimal_places=2)
+
+	def __str__(self):
+		return f"{self.name}: ${self.price}"
+
+class DinnerPlatter(models.Model):
+	LARGE = 'Large'
+	SMALL = 'Small'
+	SIZE_CHOICES = (
+		(LARGE, 'Large'),
+		(SMALL, 'Small'),
+	)
+
+	name = models.CharField(max_length=100)
+	price = models.DecimalField(max_digits=5, decimal_places=2)
+	size = models.CharField(max_length=5, choices=SIZE_CHOICES, default=SMALL)
+
+	def __str__(self):
+		return f"{self.name} ({self.size}): ${self.price}"
