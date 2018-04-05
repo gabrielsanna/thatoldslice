c4b4438fd9bc271cf2cfca45ce862e5f0d1d1aaf
Gabriel Sanna <gabrielsanna1@gmail.com>
2018-04-05 19:22:03 -0400

    Set up a login page

--- orders/urls.py
+++ orders/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("login/", auth_views.login, name="login"),
+    path("logout/", auth_views.login, name="logout"),
 ]
