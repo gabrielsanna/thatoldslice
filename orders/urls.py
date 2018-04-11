97cc0a231b68f22dc250a7ff8c974e1a9d771198
Gabriel Sanna <gabrielsanna1@gmail.com>
2018-04-11 18:00:08 -0400

    Start shopping cart feature

--- orders/urls.py
+++ orders/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path("logout/", auth_views.logout, name="logout"),
     path("register/", views.register, name="register"),
     path("menu/", views.menu, name="menu"),
+    path("orders/", views.orders, name="orders"),
+    path("order/<int:order_id>", views.single_order, name="single order"),
+    path("cart/", views.cart, name="cart"),
 ]
